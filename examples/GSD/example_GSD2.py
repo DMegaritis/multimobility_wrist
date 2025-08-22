e4dc0f9aaba5e65246c3a3e50d7a262a3af11659
dmegaritis <dmgrei3@gmail.com>
2025-08-22 10:38:37 +0100

    removed unused import statements

--- examples/GSD/example_GSD2.py
+++ examples/GSD/example_GSD2.py
@@ -1,6 +1,5 @@
 from multimob.utils.data_loader import load_imu_data
 from multimob.GSD.GSD2 import HickeyGSD
-import matplotlib.pyplot as plt
 
 """
 This is an example on how to use the Hickey algo to detect gait events while having a compatible output with my pipeline.
