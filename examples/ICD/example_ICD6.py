e2511cd666426638dbf640b1d2955c7f645edcd9
dmegaritis <dmgrei3@gmail.com>
2025-09-16 09:12:47 +0100

    comment update

--- examples/ICD/example_ICD6.py
+++ examples/ICD/example_ICD6.py
@@ -10,7 +10,7 @@ imu_data = load_imu_data()
 # only one bout of walking
 imu_data = imu_data[962:1427]
 
-# Create an instance of the EncarnaIC class
+# Create an instance of the GuIC class
 ICs = GuIC().detect(imu_data, sampling_rate_hz=100)
 
 print(ICs.ic_list_)
